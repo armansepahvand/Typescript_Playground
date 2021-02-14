0f1c5d462e9acddbd49284077fec328e29fea961
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-02-14 14:54:49 -0700

    Arrays, Classes and object literal Annotation and Inference Practice

--- features/variables.ts
+++ features/variables.ts
@@ -6,3 +6,25 @@ let speed: string="fast";
 
 let nothingMuch: null =null;
 let nothing: undefined;
+
+
+//Arrays Annotation and ionference
+let colors:string[] = ["red", "green", "blue"];
+let myNumbers: number[] = [2,4,6]
+
+//Classes Annotation and ionference
+class Car{
+
+}
+
+let car:Car = new Car();
+
+//Object literal Annotation and ionference
+
+let point: {x:number; y:number}={
+    x:10,
+    y:20
+};
+
+
+
