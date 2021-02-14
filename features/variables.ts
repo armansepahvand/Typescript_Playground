00f88cff982823a7ceaaf356f3c5f5a6d176458b
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-02-14 15:50:26 -0700

    When to use annotations

--- features/variables.ts
+++ features/variables.ts
@@ -32,4 +32,31 @@ const logNumbr: (i:number)=>void = (i)=>{
 };
 
 
+//When to use annotations
+// 1) Function that returns the "any" type
+const json= '{"x":10, "y":20}';
+const coordinates:{x:number; y:number} = JSON.parse(json);
+console.log(coordinates);
+
+// 2) When we declare a variable on one line
+//and iniotialize it later
+
+let words= ["red", "green", "blue"];
+let foundWord: boolean;
+
+for(let i=0; i<words.length; i++){
+    if (words[i]==='green'){
+        foundWord=true;
+    }
+}
+
+// 3) Variable whos type cannot be inferred correctly
+let numbers= [-10,-1,3];
+let numberAboveZero: boolean | number = false;
+for(let i=0; i<numbers.length; i++){
+    if (numbers[i]>0){
+        numberAboveZero=numbers[i];
+    }
+}
+
 
