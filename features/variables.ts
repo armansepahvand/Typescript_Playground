83f9eabbd09792ffa4ec16a145525e2bda6e3e49
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-02-14 14:58:31 -0700

    Functions Annotation example

--- features/variables.ts
+++ features/variables.ts
@@ -8,22 +8,27 @@ let nothingMuch: null =null;
 let nothing: undefined;
 
 
-//Arrays Annotation and ionference
+//Arrays Annotation and inference
 let colors:string[] = ["red", "green", "blue"];
 let myNumbers: number[] = [2,4,6]
 
-//Classes Annotation and ionference
+//Classes Annotation and inference
 class Car{
 
 }
 
 let car:Car = new Car();
 
-//Object literal Annotation and ionference
+//Object literal Annotation and inference
 
 let point: {x:number; y:number}={
     x:10,
     y:20
+};
+
+//Function Annotation and inference
+const logNumbr: (i:number)=>void = (i)=>{
+    console.log(i);
 };
 
 
