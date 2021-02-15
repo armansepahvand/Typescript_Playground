05e859d61096d1da058525c6fcaa1ce3a70aec82
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-02-14 21:08:24 -0700

    Created Company Class

---
+++ Maps/src/Company.ts
@@ -0,0 +1,21 @@
+//import faker library to create random values for Company Class
+import faker from "faker";
+
+//Define  the Company Class
+export class Company{
+    companyName:string;
+    catchPhrase: string;
+    location:{
+        lat:number;
+        lng:number;
+    };
+
+    constructor(){
+        this.companyName=faker.company.companyName(),
+        this.catchPhrase=faker.company.catchPhrase(),
+        this.location={
+            lat: parseFloat(faker.address.latitude()),
+            lng: parseFloat(faker.address.longitude())
+        }
+    }
+}
