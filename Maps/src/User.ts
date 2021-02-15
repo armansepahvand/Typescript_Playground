0f6b853be91de98134c6391ca64f62acd46238ea
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-02-14 20:02:41 -0700

    Created User class using faker library to generate random values

--- Maps/src/User.ts
+++ Maps/src/User.ts
@@ -1,3 +1,19 @@
+//import faker library to create random values for User Class
+import faker from "faker";
+
+//Define  the User Class
 class User{
-    
+    name:string;
+    location:{
+        lat:number;
+        lng:number;
+    };
+
+    constructor(){
+        this.name=faker.name.firstName();
+        this.location={
+            lat: parseFloat(faker.address.latitude()),
+            lng: parseFloat(faker.address.longitude())
+        }
+    }
 }
