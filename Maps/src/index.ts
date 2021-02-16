3d5b299cda1789f48fb078f37754a3ca72ae7c1e
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-02-16 10:38:18 -0700

    used the CustomMap class to show the map on browser

--- Maps/src/index.ts
+++ Maps/src/index.ts
@@ -1,5 +1,6 @@
 // import {User} from "./User";
 // import { Company } from "./Company";
+import {CustomMap} from "./CustomMap";
 
 // const user= new User();
 // const company= new Company();
@@ -7,10 +8,5 @@
 // console.log(user);
 // console.log(company);
 
-const map=new google.maps.Map(document.getElementById('map'),{
-    zoom:1,
-    center:{
-        lat:0,
-        lng:0
-    }
-});
+const map= new CustomMap();
+
