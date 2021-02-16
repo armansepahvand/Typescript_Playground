f9c0bb2329227d4714a5162384a67427f611a39b
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-02-16 10:35:59 -0700

    made the googleMap property private

--- Maps/src/CustomMap.ts
+++ Maps/src/CustomMap.ts
@@ -1,7 +1,7 @@
 
 export class CustomMap{
 
-    googleMap:google.maps.Map;
+    private googleMap:google.maps.Map;
 
     constructor(){
         this.googleMap=new google.maps.Map(document.getElementById('map'),{
