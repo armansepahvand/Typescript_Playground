7b9a2c96a94522ee0d2e19a121a45a36da93d09c
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-02-16 13:21:32 -0700

    added a Mappable interface to be able to pass any class that has lat and lng properties to the addMarker method

--- Maps/src/CustomMap.ts
+++ Maps/src/CustomMap.ts
@@ -1,3 +1,14 @@
+import {User} from "./User";
+import {Company} from "./Company";
+
+//Interface to instruct every other class 
+// on How they can be an argument to "addMarker"
+interface Mappable{
+    location:{
+        lat:number;
+        lng:number;
+    }
+}
 
 export class CustomMap{
 
@@ -5,13 +16,27 @@ export class CustomMap{
 
     constructor(divId:string){
         this.googleMap=new google.maps.Map(document.getElementById(divId),{
-            zoom:1,
+            zoom:3,
             center:{
                 lat:0,
                 lng:0
             }
+           
         });
+        
     }   
 
+    addMarker(mappable:Mappable):void {
+        new google.maps.Marker({
+            map:this.googleMap,
+            position:{
+                lat:mappable.location.lat,
+                lng:mappable.location.lng,
+            }
+        })
+    }
+    
+
 }
+
 
