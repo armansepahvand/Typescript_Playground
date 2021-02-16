190b0a85e27593e68974fd63d88a17b8352f839e
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-02-16 10:34:45 -0700

    Created a curmmap class to encapsuilate the map generation logig

---
+++ Maps/src/CustomMap.ts
@@ -0,0 +1,17 @@
+
+export class CustomMap{
+
+    googleMap:google.maps.Map;
+
+    constructor(){
+        this.googleMap=new google.maps.Map(document.getElementById('map'),{
+            zoom:1,
+            center:{
+                lat:0,
+                lng:0
+            }
+        });
+    }   
+
+}
+
